36be0677bd2a3239a6ce15ae3040348ea7b9334c
redrookie <justgamessites@hotmail.com>
2022-05-05 15:42:36 -0300

    Refactor no código

--- modules/findClosestValidDate.js
+++ modules/findClosestValidDate.js
@@ -3,21 +3,19 @@ function findClosestValidDate(minDate, maxDate, data) {
 		return minDate;
 	}
 	let currDate = new Date(minDate);
-	let auxDate = new Date(currDate.getTime());
+	let auxDate = new Date(currDate);
 	let maxDateLimit = new Date(maxDate);
 	currDate.setDate(auxDate.getDate() + 1);
-	let difference =
-		(maxDateLimit.getTime() - currDate.getTime()) / (1000 * 3600 * 24);
+	let difference = (maxDateLimit - currDate) / (1000 * 3600 * 24);
 	while (
 		!data.hasOwnProperty(currDate.toISOString().split("T")[0]) &&
 		difference > 0
 	) {
-		auxDate = new Date(currDate.getTime());
+		auxDate = new Date(currDate);
 		currDate.setDate(auxDate.getDate() + 1);
-		difference =
-			(maxDateLimit.getTime() - currDate.getTime()) / (1000 * 3600 * 24);
+		difference = (maxDateLimit - currDate) / (1000 * 3600 * 24);
 	}
-	return currDate;
+	return currDate.toISOString().split("T")[0];
 }
 
 export default findClosestValidDate;
